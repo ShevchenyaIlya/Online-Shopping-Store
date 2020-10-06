e865dac7255679f2e1aa8a9956c44b0e568cbd81
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-10-06 12:13:07 +0300

    ProductStore/Controllers

---
+++ ProductStore/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProductStore.Data;
+using ProductStore.Models;
+
+namespace ProductStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private AuthDbContext _context;
+
+        public HomeController(AuthDbContext context, ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var products = _context.Users;
+            // Logging levels
+            //_logger.LogTrace("This is our first logged message Trace");
+            //_logger.LogDebug("This is our first logged message Debug");
+            //_logger.LogInformation("This is our first logged message Information");
+            //_logger.LogWarning("This is our first logged message Warning");
+            //_logger.LogError("This is our first logged message Error");
+            //_logger.LogCritical("This is our first logged message Critical");
+
+
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ProductStore/Controllers/MarksController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProductStore.Data;
+using ProductStore.Models;
+
+namespace ProductStore.Controllers
+{
+    public class MarksController : Controller
+    {
+        private readonly AuthDbContext _context;
+
+        public MarksController(AuthDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Marks
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Mark.ToListAsync());
+        }
+
+        // GET: Marks/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mark = await _context.Mark
+                .FirstOrDefaultAsync(m => m.MarkId == id);
+            if (mark == null)
+            {
+                return NotFound();
+            }
+
+            return View(mark);
+        }
+
+        // GET: Marks/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Marks/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MarkId,TotalMark")] Mark mark)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(mark);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(mark);
+        }
+
+        // GET: Marks/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mark = await _context.Mark.FindAsync(id);
+            if (mark == null)
+            {
+                return NotFound();
+            }
+            return View(mark);
+        }
+
+        // POST: Marks/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MarkId,TotalMark")] Mark mark)
+        {
+            if (id != mark.MarkId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(mark);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MarkExists(mark.MarkId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(mark);
+        }
+
+        // GET: Marks/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mark = await _context.Mark
+                .FirstOrDefaultAsync(m => m.MarkId == id);
+            if (mark == null)
+            {
+                return NotFound();
+            }
+
+            return View(mark);
+        }
+
+        // POST: Marks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var mark = await _context.Mark.FindAsync(id);
+            _context.Mark.Remove(mark);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MarkExists(int id)
+        {
+            return _context.Mark.Any(e => e.MarkId == id);
+        }
+    }
+}
