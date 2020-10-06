bae323bbe2c105d9924cf479a84cf840f61710a7
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-10-06 12:13:56 +0300

    Properties and context files.

---
+++ ProductStore/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProductStore.Areas.Identity.Data;
+using ProductStore.Data;
+
+[assembly: HostingStartup(typeof(ProductStore.Areas.Identity.IdentityHostingStartup))]
+namespace ProductStore.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireDigit = true;
+                    options.Password.RequireLowercase = true;
+                    options.Password.RequireUppercase = true;
+                    options.Password.RequiredLength = 6;
+                    options.Password.RequireNonAlphanumeric = true;
+                })
+                    .AddEntityFrameworkStores<AuthDbContext>();
+            });
+        }
+    }
+}
---
+++ ProductStore/Data/AuthDbContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ProductStore.Areas.Identity.Data;
+using ProductStore.Models;
+
+namespace ProductStore.Data
+{
+    public class AuthDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options)
+            : base(options)
+        {
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Country> Country { get; set; }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<ProductBasket> ProductBasket { get; set; }
+        public DbSet<Rating> Rating { get; set; }
+        public DbSet<Sale> Sale { get; set; }
+        public DbSet<Mark> Mark { get; set; }
+        public DbSet<Comment> Comment { get; set; }
+    }
+}
