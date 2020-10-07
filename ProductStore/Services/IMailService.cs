a7109b955a507657b17972923ac1096e84d9c61e
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-10-07 23:01:38 +0300

    Interface and class for email sending.

---
+++ ProductStore/Services/IMailService.cs
@@ -0,0 +1,15 @@
+﻿using ProductStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductStore.Services
+{
+    public interface IMailService
+    {
+        Task SendEmailAsync(MailRequest mailRequest);
+        Task SendWelcomeEmailAsync(WelcomeRequest request);
+        Task SendVerifiedEmailAsync(WelcomeRequest request, string token, bool flag);
+    }
+}
