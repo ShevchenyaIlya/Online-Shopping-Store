fe8e3a456bd1006b2af7aef287ff9cc7458fc9f4
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-10-07 08:17:33 +0300

    Update migration

---
+++ ProductStore/Data/Migrations/20201006190338_Image-For-User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProductStore.Migrations
+{
+    public partial class ImageForUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ProfilePicture",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UsernameChangeLimit",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfilePicture",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UsernameChangeLimit",
+                table: "AspNetUsers");
+        }
+    }
+}
