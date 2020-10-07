e280af62c7c9a308e83155dfad83e1fad86726d7
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-10-07 08:16:01 +0300

    Chnage configuration files: Program.cs and Startup.cs

--- ProductStore/Startup.cs
+++ ProductStore/Startup.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ProductStore.Areas.Identity.Data;
 using ProductStore.Data;
 
 namespace ProductStore
@@ -25,6 +27,11 @@ namespace ProductStore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddDbContext<AuthDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            //services.AddIdentity<ApplicationUser, IdentityRole>()
+            //        .AddEntityFrameworkStores<AuthDbContext>()
+            //        .AddDefaultUI()
+            //        .AddDefaultTokenProviders();
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
