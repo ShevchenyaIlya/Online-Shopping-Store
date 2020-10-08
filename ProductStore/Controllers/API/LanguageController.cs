f78120d2629892c70e3b606ecfdb3698ca5078ff
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-10-08 14:48:18 +0300

    Controller for handling Language changies

---
+++ ProductStore/Controllers/API/LanguageController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace ProductStore.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LanguageController : ControllerBase
+    {
+        private readonly IStringLocalizer<LanguageController> _localizer;
+        public LanguageController(IStringLocalizer<LanguageController> localizer)
+        {
+            _localizer = localizer;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var guid = Guid.NewGuid();
+            var value = _localizer["RandomGUID", guid.ToString()].Value;
+            return Ok(value);
+        }
+    }
+}
