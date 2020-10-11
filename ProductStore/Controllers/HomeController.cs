44b4e4de732c6833164fb46de5a5e4cef76b820c
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-10-11 21:00:23 +0300

    Little changies in source files

--- ProductStore/Controllers/HomeController.cs
+++ ProductStore/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using ProductStore.Data;
 using ProductStore.Models;
@@ -22,9 +23,9 @@ namespace ProductStore.Controllers
             _context = context;
         }
 
-        public IActionResult Index()
+        [AllowAnonymous]
+        public async Task<IActionResult> Index()
         {
-            var products = _context.Users;
             // Logging levels
             //_logger.LogTrace("This is our first logged message Trace");
             //_logger.LogDebug("This is our first logged message Debug");
@@ -34,7 +35,105 @@ namespace ProductStore.Controllers
             //_logger.LogCritical("This is our first logged message Critical");
 
 
+            var products = await _context.Products.OrderByDescending(m => m.AddedDate).ToListAsync();
+            ViewData["categories"] = _context.Category;
+            return View(products);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult Image()
+        {
+            ViewBag.image = _context.Products.ToList()[0].ProductPicture;
+            return View(_context.Products.ToList());
+        }
+
+        [Authorize]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
             return View();
+        }
+
+        public async Task<ActionResult> ProductCard(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.Include(n => n.Category)
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        public async Task<ActionResult> CategoryCard(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Category
+                .FirstOrDefaultAsync(m => m.CategoryId == id);
+
+            var products = await _context.Products.Where(m => m.Category.CategoryId == category.CategoryId).ToListAsync();
+            ViewBag.Products = products;
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [AllowAnonymous]
+        public ActionResult Products()
+        {
+            var products = _context.Products.ToList<Product>();
+            return View(products);
+        }
+        [AllowAnonymous]
+        public ActionResult Category()
+        {
+            var categories = _context.Category.ToList<Category>();
+            return View(categories);
+        }
+
+        [AllowAnonymous]
+        public ActionResult CategoryItems(int? id)
+        {
+            var products = _context.Products.ToList<Product>();
+            List<Product> categoryProducts = new List<Product>();
+            foreach (var product in products)
+            {
+                if (product.Category.CategoryId == id)
+                {
+                    categoryProducts.Add(product);
+                }
+            }
+
+            ViewBag.categoryProducts = categoryProducts;
+
+            return View(products);
+        }
+
+        [AllowAnonymous]
+        public ActionResult FindProduct(string searching)
+        {
+            var products = from product in _context.Products
+                           select product;
+
+            if (!String.IsNullOrEmpty(searching))
+            {
+                products = products.Where(product => product.ProductName.Contains(searching));
+            }
+
+            return View(products.ToList());
         }
 
         [Authorize]
