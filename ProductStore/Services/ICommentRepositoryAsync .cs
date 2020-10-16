969e66aa101060dc857bcdf716604efe48059d62
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-10-16 09:11:50 +0300

    Interfaces for realization AJAX communication

---
+++ ProductStore/Services/ICommentRepositoryAsync .cs
@@ -0,0 +1,13 @@
+﻿using ProductStore.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductStore.Services
+{
+    public interface ICustomerRepositoryAsync : IGenericRepositoryAsync<Comment>
+    {
+
+    }
+}
