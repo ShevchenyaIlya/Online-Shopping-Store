ce27740a8222d92a148fe3403a02abf4ff9b0086
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-10-16 09:10:40 +0300

    ProductStore/Repositories

---
+++ ProductStore/Repositories/GenericRepositoryAsync.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductStore.Data;
+using ProductStore.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductStore.Repositories
+{
+    public class GenericRepositoryAsync<T> : IGenericRepositoryAsync<T> where T : class
+    {
+        private readonly AuthDbContext _dbContext;
+        public GenericRepositoryAsync(AuthDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public virtual async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbContext.Set<T>().FindAsync(id);
+        }
+        public async Task<T> AddAsync(T entity)
+        {
+            await _dbContext.Set<T>().AddAsync(entity);
+            return entity;
+        }
+        public Task UpdateAsync(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            return Task.CompletedTask;
+        }
+        public Task DeleteAsync(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            return Task.CompletedTask;
+        }
+        public async Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            return await _dbContext.Set<T>().ToListAsync();
+        }
+    }
+}
