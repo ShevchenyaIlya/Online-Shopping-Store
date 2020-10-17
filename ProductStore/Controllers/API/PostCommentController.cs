bd57fc9c16ada57385ea96a2ce4ba5abddfea687
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-10-17 15:11:13 +0300

    Post comment api controller

---
+++ ProductStore/Controllers/API/PostCommentController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ProductStore.Data;
+using ProductStore.Models;
+
+namespace ProductStore.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostCommentController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+        private AuthDbContext _context;
+
+        public PostCommentController(AuthDbContext context, ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IEnumerable<Comment> GetAllComments()
+        {
+            return  _context.Comment.Include(m => m.CommentProduct).Include(m => m.CommentUser).ToList();
+        }
+
+    }
+}
