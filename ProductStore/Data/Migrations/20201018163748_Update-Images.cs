d72ac8a0bb81ccc602229964673d81602d3c28d7
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-10-19 12:15:53 +0300

    New migrations for changing files

---
+++ ProductStore/Data/Migrations/20201018163748_Update-Images.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProductStore.Migrations
+{
+    public partial class UpdateImages : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Category");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProductPicture",
+                table: "Products",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageName",
+                table: "Products",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CountryPicture",
+                table: "Country",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CategoryPicture",
+                table: "Category",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageName",
+                table: "Category",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageName",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ImageName",
+                table: "Category");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "ProductPicture",
+                table: "Products",
+                type: "varbinary(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "CountryPicture",
+                table: "Country",
+                type: "varbinary(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "CategoryPicture",
+                table: "Category",
+                type: "varbinary(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Category",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
