99d5b52e88c387fb4c8992054f6f42512b12db72
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-10-19 12:13:29 +0300

    Custom service for storing images

---
+++ ProductStore/Services/ImageService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductStore.Services
+{
+    public class ImageService
+    {
+        private readonly IWebHostEnvironment _appEnvironment;
+        private readonly IConfiguration _configuration;
+        public ImageService(IWebHostEnvironment appEnvironment, IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _appEnvironment = appEnvironment;
+        }
+        public async Task<string> SaveImageAsync(IFormFile uploadedFile)
+        {
+            if (uploadedFile != null)
+            {
+                string path = _configuration["ImagePath"] + uploadedFile.FileName;
+                using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
+                {
+                    await uploadedFile.CopyToAsync(fileStream);
+                }
+                return path;
+            }
+            return "";
+        }
+
+        public void DeleteImage(string Path)
+        {
+            if(File.Exists(_appEnvironment.WebRootPath + Path))
+                File.Delete(_appEnvironment.WebRootPath + Path);
+        }
+    }
+}
