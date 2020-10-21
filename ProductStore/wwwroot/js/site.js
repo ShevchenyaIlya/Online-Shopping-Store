55378d485bd073f0dcde61df7ca49ba3ee30bb65
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-10-21 23:35:58 +0300

    ProductStore/wwwroot/js scripts

--- ProductStore/wwwroot/js/site.js
+++ ProductStore/wwwroot/js/site.js
@@ -1,73 +1,12 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var image_field = $('.profilePicture')[0]
 
-// Write your JavaScript code.
+$('.image-uploader').on('change', function (e) {
+    var selectedFile = e.target.files[0];
+    var reader = new FileReader();
+    image_field.title = selectedFile.name;
+    reader.onload = function (event) {
+        image_field.src = event.target.result;
+    };
 
-$(document).ready(function () {
-    jQueryModalGet = (url, title) => {
-        try {
-            $.ajax({
-                type: 'GET',
-                url: url,
-                contentType: false,
-                processData: false,
-                success: function (res) {
-                    $('#form-modal .modal-body').html(res.html);
-                    $('#form-modal .modal-title').html(title);
-                    $('#form-modal').modal('show');
-                },
-                error: function (err) {
-                    console.log(err)
-                }
-            })
-            return false;
-        } catch (ex) {
-            console.log(ex)
-        }
-    }
-    jQueryModalPost = form => {
-        try {
-            $.ajax({
-                type: 'POST',
-                url: form.action,
-                data: new FormData(form),
-                contentType: false,
-                processData: false,
-                success: function (res) {
-                    if (res.isValid) {
-                        $('#viewAll').html(res.html)
-                        $('#form-modal').modal('hide');
-                    }
-                },
-                error: function (err) {
-                    console.log(err)
-                }
-            })
-            return false;
-        } catch (ex) {
-            console.log(ex)
-        }
-    }
-    jQueryModalDelete = form => {
-        if (confirm('Are you sure to delete this record ?')) {
-            try {
-                $.ajax({
-                    type: 'POST',
-                    url: form.action,
-                    data: new FormData(form),
-                    contentType: false,
-                    processData: false,
-                    success: function (res) {
-                        $('#viewAll').html(res.html);
-                    },
-                    error: function (err) {
-                        console.log(err)
-                    }
-                })
-            } catch (ex) {
-                console.log(ex)
-            }
-        }
-        return false;
-    }
-});
+    reader.readAsDataURL(selectedFile);
+})
