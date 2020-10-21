218f905c20a9e209afc33db483461cec5b11513f
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-10-21 08:22:46 +0300

    Rating View Model for handling correct star apearence.

---
+++ ProductStore/VIewModels/RatingViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductStore.VIewModels
+{
+    public class RatingViewModel
+    {
+        public double TotalRating { get; set; }
+        public int FullStars { get; set; }
+        public bool HalfStar { get; set; }
+        public int EmptyStart { get; set; }
+
+        public int MarkCount { get; set; }
+    }
+}
