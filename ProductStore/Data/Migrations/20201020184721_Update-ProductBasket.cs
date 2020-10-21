9e7b6dbe09addb43a963d87a1a38c2faea478064
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-10-21 08:15:05 +0300

    New project migrations

---
+++ ProductStore/Data/Migrations/20201020184721_Update-ProductBasket.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProductStore.Migrations
+{
+    public partial class UpdateProductBasket : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "OrderId",
+                table: "ProductBasket",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductBasket_OrderId",
+                table: "ProductBasket",
+                column: "OrderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductBasket_Order_OrderId",
+                table: "ProductBasket",
+                column: "OrderId",
+                principalTable: "Order",
+                principalColumn: "OrderId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductBasket_Order_OrderId",
+                table: "ProductBasket");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProductBasket_OrderId",
+                table: "ProductBasket");
+
+            migrationBuilder.DropColumn(
+                name: "OrderId",
+                table: "ProductBasket");
+        }
+    }
+}
