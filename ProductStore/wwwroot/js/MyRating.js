1c18c326ca9ce9eaa74080902144b5a79f13d1fc
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-10-21 08:15:59 +0300

    JavaScript file with AJAX script for putting 1 rating to product

---
+++ ProductStore/wwwroot/js/MyRating.js
@@ -0,0 +1,14 @@
+﻿
+$("#ratingValue1").click(function () {
+    var prod = '@Model.ProductName';
+    var userIdentificator = '@user.Id';
+    var ratingValue = "1";
+    $.ajax({
+        type: "get",
+        url: "/Home/OnUpdateRating",
+        success: function (responce) {
+            $("#ratinglist").html(responce);
+        },
+        data: { productName: prod, userId: userIdentificator, value: ratingValue }
+    });
+});
