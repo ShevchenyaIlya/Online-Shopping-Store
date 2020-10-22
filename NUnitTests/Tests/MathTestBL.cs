44267e19cb2b16e9e29e8ba757188d91d96a5c0c
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-10-23 00:16:03 +0300

    Unit tests with NUnit

---
+++ NUnitTests/Tests/MathTestBL.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using ProductStore.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitTests.Tests
+{
+    [TestFixture]
+    class MathTestBL
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var mathBL = new MathBL();
+            int result = mathBL.Sum(2, 4);
+            Assert.IsTrue(result == 6);
+        }
+    }
+}
