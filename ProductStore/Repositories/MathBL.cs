8c40b053cbd3611d3283044cac292213093fc7df
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-10-23 00:17:06 +0300

    Test class for adding two numbers

---
+++ ProductStore/Repositories/MathBL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductStore.Repositories
+{
+    public class MathBL
+    {
+        public int Sum(int num1, int num2)
+        {
+            int sum = num1 + num2;
+            return sum;
+        }
+    }
+}
