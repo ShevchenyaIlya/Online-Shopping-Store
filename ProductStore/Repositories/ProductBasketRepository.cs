851856a6ea89444cbc47da4f405395d2108e40ba
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-12-04 23:23:37 +0300

    ProductStore/Repositories ProductBasketRepository interface and implementation

---
+++ ProductStore/Repositories/ProductBasketRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductStore.Data;
+using ProductStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductStore.Repositories
+{
+    public class ProductBasketRepository: IProductBasketRepository
+    {
+        private readonly AuthDbContext _dbContext;
+
+        public ProductBasketRepository(AuthDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public void AddBasketToTransaction(ProductBasket basket)
+        {
+            _dbContext.Add(basket);
+        }
+    }
+}
