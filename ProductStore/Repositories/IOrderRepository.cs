bc35569e18927e8e31e905537d3b7603ab5362b5
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-12-04 23:22:37 +0300

    ProductStore/Repositories OrderRepository interface and implementation

---
+++ ProductStore/Repositories/IOrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using ProductStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductStore.Repositories
+{
+    public interface IOrderRepository
+    {
+        Task<IEnumerable<Order>> GetOrders();
+        void InsertOrder(Order order);
+        void Save();
+        void AddOrderToTransaction(Order order);
+    }
+}
---
+++ ProductStore/Repositories/OrderRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductStore.Data;
+using ProductStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductStore.Repositories
+{
+    public class OrderRepository: IOrderRepository
+    {
+        private readonly AuthDbContext _dbContext;
+
+        public OrderRepository(AuthDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<IEnumerable<Order>> GetOrders()
+        {
+            return await _dbContext.Order.Include(c => c.Customer).ToListAsync();
+        }
+        public void InsertOrder(Order order)
+        {
+            _dbContext.Add(order);
+            Save();
+        }
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public void AddOrderToTransaction(Order order)
+        {
+            _dbContext.Add(order);
+        }
+    }
+}
