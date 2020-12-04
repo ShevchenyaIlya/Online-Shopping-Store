6dec696f2ff4fc2dd4cce270b71b82d5c32dc40f
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-12-04 23:22:25 +0300

    ProductStore/Repositories CommentRepository interface and implementation

---
+++ ProductStore/Repositories/CommentRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductStore.Data;
+using ProductStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductStore.Repositories
+{
+    public class CommentRepository: ICommentRepository
+    {
+        private readonly AuthDbContext _dbContext;
+
+        public CommentRepository(AuthDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<IEnumerable<Comment>> GetProductsWhereName(string name)
+        {
+            return await _dbContext.Comment
+                .Include(m => m.CommentUser).Include(n => n.CommentProduct)
+                .Where(m => m.CommentProduct.ProductName == name).OrderByDescending(m => m.PostDate).ToListAsync(); ;
+        }
+
+        public async Task<IEnumerable<Comment>> GetProductsLimit(long id, int limit)
+        {
+            return await _dbContext.Comment.Include(m => m.CommentUser).Include(n => n.CommentProduct)
+                .Where(mbox => mbox.CommentProduct.ProductId == id).OrderByDescending(m => m.PostDate).Take(limit).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Comment>> GetComments()
+        {
+            return await _dbContext.Comment.Include(c => c.CommentUser).Include(p => p.CommentProduct).ToListAsync();
+        }
+
+        public void InsertComment(Comment comment)
+        {
+            _dbContext.Add(comment);
+            Save();
+        }
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+    }
+}
---
+++ ProductStore/Repositories/ICommentRepository.cs
@@ -0,0 +1,17 @@
+﻿using ProductStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductStore.Repositories
+{
+    public interface ICommentRepository
+    {
+        Task<IEnumerable<Comment>> GetProductsWhereName(string name);
+        Task<IEnumerable<Comment>> GetProductsLimit(long id, int limit);
+        Task<IEnumerable<Comment>> GetComments();
+        void InsertComment(Comment comment);
+        void Save();
+    }
+}
