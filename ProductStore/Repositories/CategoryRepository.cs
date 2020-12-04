bbb7d07d13437b67cb258fe5990048f759a25f81
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-12-04 23:22:53 +0300

    ProductStore/Repositories CategoryRepository interface and implementation

---
+++ ProductStore/Repositories/CategoryRepository.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductStore.Data;
+using ProductStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductStore.Repositories
+{
+    public class CategoryRepository: ICategoryRepository
+    {
+        private readonly AuthDbContext _dbContext;
+
+        public CategoryRepository(AuthDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<Category>> GetCategories()
+        {
+            return await _dbContext.Category.ToListAsync();
+        }
+
+        public bool CategoryExist(long id)
+        {
+            return _dbContext.Category.Any(e => e.CategoryId == id);
+        }
+
+        public void RemoveCategory(long categoryId)
+        {
+            var category = _dbContext.Category.Find(categoryId);
+            _dbContext.Category.Remove(category);
+            Save();
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            _dbContext.Entry(category).State = EntityState.Modified;
+            Save();
+        }
+
+        public void InsertCategory(Category category)
+        {
+            _dbContext.Add(category);
+            Save();
+        }
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public async Task<Category> FindFirsByCategoryName(string name)
+        {
+            return await _dbContext.Category.Where(value => value.CategoryName == name).FirstAsync();
+        }
+        public async Task<Category> GetCategorytByID(long? category)
+        {
+            return await _dbContext.Category.FirstOrDefaultAsync(m => m.CategoryId == category);
+        }
+    }
+}
---
+++ ProductStore/Repositories/ICategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using ProductStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductStore.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetCategories();
+
+        Task<Category> FindFirsByCategoryName(string name);
+        Task<Category> GetCategorytByID(long? category);
+        bool CategoryExist(long id);
+        void RemoveCategory(long categoryId);
+        void UpdateCategory(Category category);
+        void InsertCategory(Category category);
+        void Save();
+    }
+}
