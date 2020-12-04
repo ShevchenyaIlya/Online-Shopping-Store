3165b48597a564f8eb05657b9a337bfae1c3f071
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-12-04 23:26:22 +0300

    Store date and time in DB in UTC format

--- ProductStore/Hubs/CommentHub.cs
+++ ProductStore/Hubs/CommentHub.cs
@@ -31,19 +31,19 @@ namespace ProductStore.Hubs
 
 
         [Authorize]
-        public async Task SendMessage(string title, string body, string postDate, string image, string username, string productName)
+        public async Task SendMessage(string title, string body, string image, string username, string productName)
         {
             if (!(productName == null || title == null || body == null))
             {
                 var dataComment = new Comment();
                 dataComment.CommentTitle = title;
                 dataComment.CommentBody = body;
-                dataComment.PostDate = DateTime.Parse(postDate);
+                dataComment.PostDate = DateTime.UtcNow;
                 dataComment.CommentUser = await _context.Users.FirstOrDefaultAsync(m => m.UserName == username);
                 dataComment.CommentProduct = await _context.Products.FirstOrDefaultAsync(mbox => mbox.ProductName == productName);
                 _context.Add(dataComment);
                 await _context.SaveChangesAsync();
-                await Clients.Group(productName).SendAsync("ReceiveMessage", title, body, postDate, image, username, productName);
+                await Clients.Group(productName).SendAsync("ReceiveMessage", title, body, dataComment.PostDate.ToString(), image, username, productName);
             }
         }
     }
