763f2d66875aa62c45da572969cbb8a1517fe000
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-12-04 23:21:39 +0300

    ProductStore/Repositories SaleRepository interface and implementation

---
+++ ProductStore/Repositories/ISaleRepository.cs
@@ -0,0 +1,13 @@
+﻿using ProductStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductStore.Repositories
+{
+    public interface ISaleRepository
+    {
+        Task<IEnumerable<Sale>> GetSales();
+    }
+}
---
+++ ProductStore/Repositories/SaleRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductStore.Data;
+using ProductStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductStore.Repositories
+{
+    public class SaleRepository: ISaleRepository
+    {
+        private readonly AuthDbContext _dbContext;
+
+        public SaleRepository(AuthDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<Sale>> GetSales()
+        {
+            return await _dbContext.Sale.Include(m => m.Product).ToListAsync();
+        }
+    }
+}
