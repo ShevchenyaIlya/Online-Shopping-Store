2c63003576c51bc123b0427ab778bb73d831e02b
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-12-04 23:23:57 +0300

    ProductStore/Repositories IUserInterface

---
+++ ProductStore/Repositories/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using ProductStore.Areas.Identity.Data;
+using ProductStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductStore.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<ApplicationUser>> GetUsers();
+        Task<ApplicationUser> GetUserById(string userId);
+        Task<ApplicationUser> GetUserByName(string name);
+        void RemoveUser(string userId);
+        void Save();
+    }
+}
